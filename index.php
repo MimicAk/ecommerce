3c21b677b061b677ffa97f9df15141f4b0f3b3ab
MimicAk <muthurose2204@gmail.com>
2024-01-26 20:19:11 +0530

    User Register

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ include 'model/UserModel.php';
 
 $UserModel = new UserModel();
 
-$data = $UserModel->UserLogin();
+// $data = $UserModel->UserLogin();
 
+// echo $data;
+$name = 'Yuvaraj';
+$email = 'yuva@gmail.com';
+// $email = 'test@123.com';
+$password = '123';
+$con_p = '123';
+$data = $UserModel->UserRegister($name, $email, $password, $con_p);
 echo $data;
-
 ?>
--- model/UserModel.php
+++ model/UserModel.php
@@ -1,14 +1,15 @@
 <?php
 // Imports here
- 
+include 'config/connection.php';
 class UserModel
-{   
+{
 
     protected $con;
     protected $db;
     protected $table_name;
     protected $primary_key;
-    public function __construct(){
+    public function __construct()
+    {
         $this->con = new Connection();
         $this->db = $this->con->connect();
 
@@ -16,14 +17,8 @@ class UserModel
         $this->table_name = 'user';
         $this->primary_key = 'id';
     }
-    
-    public function Connect(){
-        include 'config/connection.php';
 
-        return $conn;
-    }
-
-        public function UserLogin()
+    public function UserLogin()
     {
         $email = 'test@123.com';
         // $email = $_POST['email'];
@@ -35,16 +30,48 @@ class UserModel
 
         $result = mysqli_query($this->db, $query);
 
-        if(mysqli_num_rows($result)>0){
+        if (mysqli_num_rows($result) > 0) {
             $check = mysqli_query($this->db, $query_check_pw);
-            if(mysqli_num_rows($check)>0){
+            if (mysqli_num_rows($check) > 0) {
                 return 'Login Successfull';
             } else {
                 return 'Check Your Password';
             }
-        }else {
+        } else {
             return 'User Doesnot Exit';
         }
+    }
+
+    public function UserRegister($name, $email, $password, $con_password)
+    {
+        // Name
+        // $name = 'yuva';
+        // $email = '';
+        // $password = '';
+        // $con_password = '';
+
+        if (isset($email)) {
+            $query = "SELECT email FROM $this->table_name WHERE email = '$email'";
+
+            $check_user = mysqli_query($this->db, $query);
+
+            if(mysqli_num_rows($check_user) > 0){
+                return "User Already Exist";
+            }elseif($password != $con_password){
+                return "Password Missmatch";
+            }
+            else{
+                // INSERT INTO `user`(`id`, `name`, `email`, `password`, `last_login`, `status`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')
+                $register_query = "INSERT INTO $this->table_name(name, email, password) VALUES ('$name','$email','$password')";
+
+                if(mysqli_query($this->db,$register_query)){
+                    return "User Register Successful";
+                }
+            }
+        }else {
+            return 'Enter Email';
+        }
+
     }
 
 }
